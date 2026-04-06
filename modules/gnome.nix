e045577e49c8a6553441895e1073b73cbe5a96e1
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-04-06 19:20:22 +0200

    Allow KDE Connect (GSConnect) through firewall
    Open TCP/UDP port range 1714-1764 for GSConnect extension.

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -57,6 +57,22 @@ in
     }
   ];
 
+  # GSConnect (KDE Connect) uses ports 1714-1764
+  networking.firewall = {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     adw-gtk3
     nautilus
