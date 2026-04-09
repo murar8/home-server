2d90f91da231825c51d9a4ec188953714d0411f0
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-04-09 09:51:06 +0200

    Inline treefmt config into formatter.nix, drop standalone treefmt.nix

--- formatter.nix
+++ formatter.nix
@@ -1,3 +1,21 @@
 { pkgs, inputs, ... }:
 
-(inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build.wrapper
+let
+  treefmt = inputs.treefmt-nix.lib.evalModule pkgs {
+    projectRootFile = "flake.nix";
+    programs = {
+      shellcheck.enable = true;
+      shfmt.enable = true;
+      prettier = {
+        enable = true;
+        includes = [ "*.hujson" ];
+      };
+      nixfmt = {
+        enable = true;
+        package = pkgs.nixfmt-rfc-style;
+        strict = true;
+      };
+    };
+  };
+in
+treefmt.config.build.wrapper
