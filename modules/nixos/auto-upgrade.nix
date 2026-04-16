d48a6ac6ceff3071e4e890b4257c449f292bbedf
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-04-16 19:26:54 +0200

    Simplify auto-upgrade flags and enable automatic reboot window
    Remove --recreate-lock-file and --no-write-lock-file flags (unnecessary
    for github: flake refs) and add allowReboot with a 04:00–06:00 window.

--- modules/nixos/auto-upgrade.nix
+++ modules/nixos/auto-upgrade.nix
@@ -4,12 +4,13 @@
   system.autoUpgrade = {
     enable = true;
     flake = "github:murar8/home-server#${config.networking.hostName}";
-    flags = [
-      "--recreate-lock-file"
-      "--no-write-lock-file"
-      "-L"
-    ];
+    flags = [ "-L" ];
     dates = "04:00";
     randomizedDelaySec = "45min";
+    allowReboot = true;
+    rebootWindow = {
+      lower = "04:00";
+      upper = "06:00";
+    };
   };
 }
